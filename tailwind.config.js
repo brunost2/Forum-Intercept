27785260adde1c2f9218844d82888588cd54406f
brunost2 <bruno.santos421@gmail.com>
2023-10-29 13:29:44 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        950: "#1e1e1e",
+        "placeholder-white": "#4d4d4d",
+        cinza: "#929191",
+        azul: "#0c4672",
+        esbranquicado: "#D9D9D9",
+      },
+      spacing: {
+        "100ml": "600px",
+        101: "900px",
+        300: "280px",
+      },
+      fontSize: {
+        "xxs": '0.65rem',
+      }
+    },
+  },
+  plugins: [],
+};
